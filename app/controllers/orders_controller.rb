9f5a8167ce3b05a3510293e538eef6c09d3bea6b
guillaume <gueyome@hotmail.fr>
2019-11-28 11:58:34 +0100

    stripe ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,12 +9,32 @@ class OrdersController < ApplicationController
   end
 
   def create
-    user_id = current_user.id
-    cart_id = Cart.where(user_id: user_id).last.id
-    @order = Order.create(cart_id: cart_id, date: DateTime.now)
-    @cart = Cart.create(user_id: current_user.id)
+
+    puts @amount = params[:price].to_i * 100
+    puts @user_id = current_user.id
+    puts @cart_id = Cart.where(user_id: @user_id).last.id
+    
+    customer = Stripe::Customer.create({
+    email: params[:stripeEmail],
+    source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd',
+      })
+
+    @order = Order.create(cart_id: @cart_id, date: DateTime.now)
+    @cart = Cart.create(user_id: @user_id)
     flash[:success]= "Votre commande a bien été passé"
     redirect_to root_path
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to user_cart_path(@user_id, @cart_id)
+
   end
 
   def edit
