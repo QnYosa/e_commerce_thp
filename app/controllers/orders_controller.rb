1ce9d3fb208012579964af38136d40c22eb9c997
guillaume <gueyome@hotmail.fr>
2019-11-28 10:29:19 +0100

    order sans stripe ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,10 +9,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    cart_id = params["cart_id"]
-    user_id = Cart.find(cart_id).user_id
-    @order = Order.new(cart_id: cart_id, date: DateTime.now)
+    user_id = current_user.id
+    cart_id = Cart.where(user_id: user_id).last.id
+    @order = Order.create(cart_id: cart_id, date: DateTime.now)
     @cart = Cart.create(user_id: current_user.id)
+    flash[:success]= "Votre commande a bien été passé"
+    redirect_to root_path
   end
 
   def edit
